eeef310d8f5279036881f6e786b4c88ae1a63cd1
Leandre GIAMMONA <leandre.giammonapens.uvsq.fr>
2021-04-09 09:37:18 +0200

    projet début

--- Projet.py
+++ Projet.py
@@ -19,37 +19,57 @@ import tkinter as tk
 
 #Definitions des contantes :
 
+WIDTH, HEIGHT = 500, 800
+
 #Defintions des fonctions :
 
 def Avance_Serpent():
     pass
 
+
 def Position_Pomme() :
     """génération de la première pomme et quand celle ci est mangée une autre apparait à une position aléatoire"""
     pass
+
 
 def Vitesse() :
     """Creation d'un bouton permettant de changer la vitesse en trois vitesses différentes"""
     pass
 
+
 def Echec():
     """si le serpent rentre dans un mur ou dans sa propre queue la partie est perdue"""
     pass
+
 
 def Grandir_Serpent() :
     """quand le serpent mange une pomme il grandit d'une unité"""
     pass
 
+
 def Start() :
     """appuyer sur un bouton ou une touche pour démarrer la simulation"""
     pass
 
+
 def Entre_Pseudo() :
+    """A chaque début de partie le joueur doit rentrer un pseudo"""
     pass
 
 def Score() :
+    """le score est affiché sur une partie de l'écran"""
     pass
+
 
 def Score_texte() :
+    """le score est enregistré dans un fichier .txt"""
     pass
 
+# Programme principal
+
+racine = tk.Tk()
+racine.title("simulation de la propagation d'un incendie")
+
+canvas = tk.Canvas(width = WIDTH, height = HEIGHT, bg = 'white')
+
+racine.mainloop()
