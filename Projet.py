8d95e5af61acf928db1d82b517430395969b1b2b
Leandre GIAMMONA <leandre.giammonapens.uvsq.fr>
2021-04-09 09:29:44 +0200

    projet

---
+++ Projet.py
@@ -0,0 +1,55 @@
+################
+# Auteurs:
+# LEFEBVRE Elora
+# GAY Arnaud
+# GIAMMONA Leandre
+# Nicolas Mimaud
+# REBATI Rayan
+# PEREZ Martin
+#
+# Groupe 2
+# MPCI TD2
+# dépot Github:
+# https://github.com/uvsq22001055/Projet_2_groupe2_TD2_snake.git
+################
+
+#Import des librairies :
+
+import tkinter as tk
+
+#Definitions des contantes :
+
+#Defintions des fonctions :
+
+def Avance_Serpent():
+    pass
+
+def Position_Pomme() :
+    """génération de la première pomme et quand celle ci est mangée une autre apparait à une position aléatoire"""
+    pass
+
+def Vitesse() :
+    """Creation d'un bouton permettant de changer la vitesse en trois vitesses différentes"""
+    pass
+
+def Echec():
+    """si le serpent rentre dans un mur ou dans sa propre queue la partie est perdue"""
+    pass
+
+def Grandir_Serpent() :
+    """quand le serpent mange une pomme il grandit d'une unité"""
+    pass
+
+def Start() :
+    """appuyer sur un bouton ou une touche pour démarrer la simulation"""
+    pass
+
+def Entre_Pseudo() :
+    pass
+
+def Score() :
+    pass
+
+def Score_texte() :
+    pass
+
