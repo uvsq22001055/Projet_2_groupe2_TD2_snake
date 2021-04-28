a9f09ced97dc881ba03f035a8398ac713de85b05
uvsq22001055 <elora.lefebvre@ens.uvsq.fr>
2021-04-28 13:43:21 +0200

    e

--- Projet.py
+++ Projet.py
@@ -14,7 +14,7 @@
 ################
 
 #Import des librairies :
-
+from PIL import Image, ImageTk
 import tkinter as tk
 import random as rd
 
@@ -36,6 +36,11 @@ SERPENT = 3
 
 case = [[0 for row in range(ROW)] for col in range(COL)]
 etat = [[FOND for row in range(ROW)] for col in range(COL)]
+SPEED_GAME_SLOW = 2500
+SPEED_GAME_MEDIUM = 5000
+SPEED_GAME_FAST = 10000
+SPEED_GAME_CHOOSE = 0
+END = 1
 score = []
 compteur = []
 
@@ -45,12 +50,16 @@ def Generate_Pomme() :
     """Génération de la pomme"""
     y = rd.randint(1, ROW-1)
     x = rd.randint(1, COL-1)
-    etat[y][x] = POMME
+    if etat[y][x] != SERPENT and etat[y][x] != MUR:
+        etat[y][x] = POMME
+        canvas.create_image(x*20+10, y*20+10,image=image_pomme)
+    else:
+        Generate_Pomme()
 
 
 def Generate_Serpent():
     """Génération du serpent"""
-    
+    pass
 
 
 def base():
@@ -88,19 +97,45 @@ def draw():
                 canvas.itemconfig(case[y][x], fill=coul)
             elif etat[y][x] == MUR:
                 coul = COULEUR_MUR
-                canvas.itemconfig(case[y][x], fill=coul)
-            if etat[y][x] == POMME:
-                coul = COULEUR_POMME
-                canvas.itemconfig(case[y][x], fill=coul)
+                canvas.create_image(x*20+10, y*20+10, image=image_mur)
 
 
 def Avance_Serpent():
-    score()
-
-
-def Vitesse() :
-    """Creation d'un bouton permettant de changer la vitesse en trois vitesses différentes"""
     pass
+
+    
+
+def Fast() :
+    """Creation d'un bouton permettant de changer la vitesse en rapide"""
+    global SPEED_GAME_FAST
+    f = "vitesse : rapide"
+    message_vitesse.configure(text = f)
+    id_Game = canvas.after(SPEED_GAME_FAST)
+
+
+def Slow():
+    """Creation d'un bouton permettant de changer la vitesse en lente"""
+    global SPEED_GAME_SLOW
+    s = "vitesse : lente"
+    message_vitesse.configure(text = s)
+    id_Game = canvas.after(SPEED_GAME_SLOW)
+
+
+def Medium():
+    """Creation d'un bouton permettant de changer la vitesse en moyenne"""
+    global SPEED_GAME_MEDIUM
+    m = "vitesse : moyenne"
+    message_vitesse.configure(text = m)
+    id_Game = canvas.after(SPEED_GAME_MEDIUM)
+
+
+def Vitesse():
+    """Creation d'un bouton permettant au joueur d'entrer une vitesse de son choix"""
+    global SPEED_GAME_CHOOSE
+    SPEED_GAME_CHOOSE = int(input("entrer une vitesse : "))
+    c = "vitesse : choisie"
+    message_vitesse.configure(text = c)
+    id_Game = canvas.after(SPEED_GAME_CHOOSE)
 
 
 def Echec():
@@ -115,12 +150,17 @@ def Grandir_Serpent() :
 
 def Start() :
     """Appuyer sur un bouton ou une touche pour démarrer la simulation"""
-    pass
+    global END, SPEED_GAME_FAST, SPEED_GAME_MEDIUM, SPEED_GAME_SLOW, SPEED_GAME_CHOOSE
+    Avance_Serpent()
+    id_Game = canvas.after(SPEED_GAME_SLOW or SPEED_GAME_FAST or SPEED_GAME_MEDIUM or SPEED_GAME_CHOOSE, Start)
+    if END == 0:
+        canvas.after_cancel(id_Game)
 
 
 def Pseudo() :
     """A chaque début de partie le joueur doit rentrer un pseudo"""
     pass
+
 
 def Score() :
     """le score est affiché sur une partie de l'écran"""
@@ -131,7 +171,10 @@ def Score() :
 
 def Score_texte() :
     """le score est enregistré dans un fichier .txt"""
-    pass
+    pseudo = input("Rentrez votre pseudo:" "\n")
+    f = open('score.txt', 'w')
+    f.write(Pseudo, score[0])
+    f.close()
 
 
 # Programme principal
@@ -149,6 +192,19 @@ canvas.grid(column = 0, row = 1, columnspan = 2)
 message_score.grid(column = 0, row = 0)
 message_vitesse.grid(column = 1, row = 0)
 
+canvas.bind_all('<KeyPress-d>', Fast)
+canvas.bind_all('<KeyPress-q>', Slow)
+canvas.bind_all('<KeyPress-s>', Medium)
+canvas.bind_all('<Return>', Start)
+canvas.bind_all('<KeyPress-v>', Vitesse)
+
+photo_pomme = Image.open("apple.png")
+image_pomme =  ImageTk.PhotoImage(photo_pomme)
+
+photo_mur = Image.open("mur.png")
+image_mur = ImageTk.PhotoImage(photo_mur)
+
 base()
+Avance_Serpent()
 
 racine.mainloop()
