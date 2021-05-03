e475be16817b0013d0be06ae34c23dd3a9a1f460
Martin PEREZ-CENIT <martin.perez-cenit@ens.uvsq.fr>
2021-05-03 10:15:23 +0200

    change vitesse

--- Projet.py
+++ Projet.py
@@ -37,9 +37,9 @@ SERPENT = 3
 
 case = [[0 for row in range(ROW)] for col in range(COL)]
 etat = [[FOND for row in range(ROW)] for col in range(COL)]
-SPEED_GAME_SLOW = 10000
-SPEED_GAME_MEDIUM = 5000
-SPEED_GAME_FAST = 2500
+SPEED_GAME_SLOW = 2000
+SPEED_GAME_MEDIUM = 1000
+SPEED_GAME_FAST = 500
 SPEED_GAME_CHOOSE = 0
 END = 1
 score = []
@@ -139,7 +139,7 @@ def Medium():
 def Vitesse():
     """Creation d'un bouton permettant au joueur d'entrer une vitesse de son choix"""
     global SPEED_GAME_CHOOSE
-    SPEED_GAME_CHOOSE = int(input("entrer une vitesse : "))
+    SPEED_GAME_CHOOSE = e1 * 1000
     c = "vitesse : choisie"
     message_vitesse.configure(text = c)
     id_Game = canvas.after(SPEED_GAME_CHOOSE)
